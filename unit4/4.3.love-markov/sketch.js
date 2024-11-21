7a7f41c9bf5e7b25d77413f6c97ab40a7666d0b5
emmalord6 <allemma100@gmail.com>
2024-11-20 19:52:54 -0500

    love.markov.exercise

--- unit4/4.3.love-markov/sketch.js
+++ unit4/4.3.love-markov/sketch.js
@@ -1,14 +1,31 @@
+// The nonsensical text produced in my exercise is from the book "The Camp Fire Girls Amid the Snow," written by Margaret Vandercook.
 
+let rm;
 
-let rm = RiTa.markov(2);
+function setup() {
+  rm = RiTa.markov(2);
 
+  let data = document.getElementById("source").innerText;
 
+  rm.addText(data);
 
-function preload() {
-  result = loadStrings('');
+  let lines = rm.generate(6);
+
+  let letter = select("#letter");
+
+  letter.child(createP('My Dearest Friend,'));
+
+  for (let l = 0; l < lines.length; l++) {
+    let paragraph = createP(lines[l]);
+    letter.child(paragraph);
+  }
+
+  letter.child(createP('Sincerely,<br>Emma'));
 }
 
-function setup(){
-
-	
+function mousePressed(){
+  letter.remove();
+  letter = createDiv();
+  letter.id("letter");
+  setup();
 }
